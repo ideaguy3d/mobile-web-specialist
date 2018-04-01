3ff5c29ca64f911b0837e049885ed9089d0382f0
Julius Alvarado <angularjs.uiux@gmail.com>
2018-03-31 22:04:59 -0700

    finished 'Hijacking Requests 3'

--- jitter/public/js/sw/index.js
+++ jitter/public/js/sw/index.js
@@ -3,17 +3,33 @@ self.addEventListener('fetch', function (event) {
     console.log("jha - event = ");
     console.log(event);
 
+    let respondWithText404andOffline = function () {
+        return fetch(event.request).then(function (response) {
+            if (response.status === 404) {
+                // respond with gif
+                return new Response("jha - that page does not exist");
+            }
+            return response;
+        }).catch(function () {
+            return new Response("jha - We're in offline mode ...");
+        })
+    };
+
+    let respondWithGif = function () {
+        return fetch(event.request)
+            .then(function(response){
+                if(response.status === 404) return fetch('/imgs/dr-evil.gif');
+                return response;
+            })
+            .catch(function () {
+                return new Response("jha - We're in offline mode");
+            });
+    };
+
     // catch 404's and offline
     event.respondWith(
         // param 1
-        fetch(event.request).then(function(response){
-            if(response.status === 404) {
-                return new Response("jha - That page doesn't exist");
-            }
-            return response;
-        }).catch(function(){
-            return new Response("jha - We're in offline mode ...");
-        })
+        respondWithGif()
     );
 
     // if (event.request.url.endsWith('.jpg')) event.respondWith(fetch('/imgs/dr-evil.gif'));
