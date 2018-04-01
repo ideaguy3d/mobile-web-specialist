a16f2878aa3c5f4ffeb6efc539be300a3e084fc9
Julius Alvarado <angularjs.uiux@gmail.com>
2018-03-31 22:50:57 -0700

    completed 'Cache Response Quiz'

--- jitter/public/js/sw/index.js
+++ jitter/public/js/sw/index.js
@@ -1,9 +1,33 @@
-self.addEventListener('fetch', function (event) {
+self.addEventListener('install', function (event) {
+    const urlsToCache = [
+        '/',
+        'js/main.js',
+        'css/main.css',
+        'imgs/icon.png',
+        'https://fonts.googleapis.com/css?family=Dosis:400,500,600,700%7COpen+Sans:400,600,700'
+    ];
 
+    event.waitUntil(
+        // param 1
+        caches.open('wittr-static-v1').then(function (cache) {
+            // addAll() returns a promise
+            return cache.addAll([
+                '/',
+                'js/main.js',
+                'css/main.css',
+                'imgs/icon.png',
+                'https://fonts.googleapis.com/css?family=Dosis:400,500,600,700%7COpen+Sans:400,600,700'
+            ]);
+        })
+    );
+});
+
+self.addEventListener('fetch', function (event) {
     console.log("jha - event = ");
     console.log(event);
 
-    let respondWithText404andOffline = function () {
+    //-- event.respondWith() functions:
+    let text = function () {
         return fetch(event.request).then(function (response) {
             if (response.status === 404) {
                 // respond with gif
@@ -14,23 +38,30 @@ self.addEventListener('fetch', function (event) {
             return new Response("jha - We're in offline mode ...");
         })
     };
-
-    let respondWithGif = function () {
+    let replacingJpgs = function () {
+        if (event.request.url.endsWith('.jpg')) event.respondWith(fetch('/imgs/dr-evil.gif'));
+    };
+    let gifs = function () {
         return fetch(event.request)
-            .then(function(response){
-                if(response.status === 404) return fetch('/imgs/dr-evil.gif');
+            .then(function (response) {
+                if (response.status === 404) return fetch('/imgs/dr-evil.gif');
                 return response;
             })
             .catch(function () {
                 return new Response("jha - We're in offline mode");
             });
     };
+    let myCache = function(){
+        return caches.match(event.request).then(function(response){
+            if(response) return response;
+            
+            // sort of a magic line of code that'll return the cache if offline.
+            return fetch(event.request);
+        });
+    };
 
-    // catch 404's and offline
     event.respondWith(
         // param 1
-        respondWithGif()
+        myCache()
     );
-
-    // if (event.request.url.endsWith('.jpg')) event.respondWith(fetch('/imgs/dr-evil.gif'));
 });
