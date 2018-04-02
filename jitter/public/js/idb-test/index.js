809f9706ae7a342f7a42d4615a139f94f58b982d
Julius Alvarado <angularjs.uiux@gmail.com>
2018-04-02 00:10:11 -0700

    finished other 1/2 of 'Getting Started with IDB' the quiz portion

--- jitter/public/js/idb-test/index.js
+++ jitter/public/js/idb-test/index.js
@@ -1,8 +1,16 @@
 import idb from 'idb';
 
-let dbPromise = idb.open('jtest-db', 1, function (upgradeDb) {
-    let keyValStore = upgradeDb.createObjectStore('jkeyval');
-    keyValStore.put('earth', 'ello');
+let dbPromise = idb.open('jtest-db', 3, function (upgradeDb) {
+    switch (upgradeDb.oldVersion) {
+        case 0:
+            let keyValStore = upgradeDb.createObjectStore('jkeyval');
+            keyValStore.put('earth', 'ello');
+        case 1:
+            upgradeDb.createObjectStore('people', {keyPath: 'name'});
+        case 2:
+            let peopleStore = upgradeDb.transaction.objectStore('people');
+            peopleStore.createIndex('animal', 'favoriteAnimal');
+    }
 });
 
 dbPromise.then(function (db) {
@@ -13,11 +21,67 @@ dbPromise.then(function (db) {
     console.log('jha - zValue of "ello" = ', val);
 });
 
-dbPromise.then(function(db){
+dbPromise.then(function (db) {
     let tx = db.transaction('jkeyval', 'readwrite');
     let keyValueStore = tx.objectStore('jkeyval');
     keyValueStore.put('Julius', 'name');
-    return tx.complete; 
-}).then(function(){
+    return tx.complete;
+}).then(function () {
     console.log('Added to jkeyval objectStore');
+});
+
+dbPromise.then(function (db) {
+    let tx = db.transaction('jkeyval', 'readwrite');
+    let keyValueStore = tx.objectStore('jkeyval');
+    keyValueStore.put('Grizzly Bear', 'favoriteAnimal');
+    return tx.complete;
+}).then(function () {
+    console.log('Added favoriteAnimal:"Grizzley Bear" to jkeyval');
+});
+
+dbPromise.then(function (db) {
+    let tx = db.transaction('people', 'readwrite');
+    let peopleStore = tx.objectStore('people');
+
+    peopleStore.put({
+        name: 'Julius Alvarado',
+        age: 29,
+        gender: 'male',
+        favoriteAnimal: 'pomeranian'
+    });
+
+    peopleStore.put({
+        name: 'Ary Aroosh',
+        age: 28,
+        gender: 'male',
+        favoriteAnimal: 'wolf'
+    });
+
+    peopleStore.put({
+        name: 'Vince Vu',
+        age: 28,
+        gender: 'male',
+        favoriteAnimal: 'bunny'
+    });
+
+    peopleStore.put({
+        name: 'Edward Palacios',
+        age: 24,
+        gender: 'male',
+        favoriteAnimal: 'birds'
+    });
+
+    return tx.complete;
+}).then(function () {
+    console.log('people added');
+});
+
+dbPromise.then(function (db) {
+    let tx = db.transaction('people');
+    let peopleStore = tx.objectStore('people');
+    let animalIndex = peopleStore.index('animal');
+
+    return animalIndex.getAll('cat');
+}).then(function (people) {
+    console.log('People: ', people);
 });
